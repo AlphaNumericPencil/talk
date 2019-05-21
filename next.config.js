af9e71266c90bce777a14755f00cae8bfc0c1e36
George Rohn <Rohn2024@gmail.com>
2019-05-21 18:14:27 -0400

    First commit

--- next.config.js
+++ next.config.js
@@ -58,6 +58,8 @@ module.exports = {
 */
 const path = require('path');
 const glob = require('glob');
+var port_number = server.listen(process.env.PORT || 3000);
+app.listen(port_number);
 
 module.exports = {
   webpack: (config, { dev }) => {
